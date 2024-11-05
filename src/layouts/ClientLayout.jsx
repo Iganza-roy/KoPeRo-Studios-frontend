68e707e5237572a5e7fe686378d1a746f0702d13
Iganza-roy <iganzaroy55@gmail.com>
2024-11-05 20:28:05 +0300

    Add comments to ClientLayout and CrewLayout for clarity

--- src/layouts/ClientLayout.jsx
+++ src/layouts/ClientLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
+// layout to handle normal user UI
 const Layout = () => {
   return (
     <>
--- src/layouts/CrewLayout.jsx
+++ src/layouts/CrewLayout.jsx
@@ -2,6 +2,8 @@ import { Outlet, useLocation } from 'react-router-dom';
 import CrewNavbar from '../Admin/CrewNavbar';
 import Footer from '../components/Footer';
 
+// Layout to handle crew UI
+
 const CrewLayout = () => {
   const location = useLocation();
 

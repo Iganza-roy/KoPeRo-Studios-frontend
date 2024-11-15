66abc26aa49ee13b6ee47ba5e1115ea939495b38
Iganza-roy <iganzaroy55@gmail.com>
2024-11-15 22:13:51 +0300

    Remove unnecessary comment in CrewLayout component

--- src/layouts/CrewLayout.jsx
+++ src/layouts/CrewLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet, useLocation } from 'react-router-dom';
 import CrewNavbar from '../Admin/CrewNavbar';
 import Footer from '../components/Footer';
 
-// Layout to handle crew UI
 
 const CrewLayout = () => {
   const location = useLocation();
